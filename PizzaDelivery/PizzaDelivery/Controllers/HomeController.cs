fa7af79a2ae16fe26849f0e8407bff6f18a577a4
Giorgi <gio_bolota@yahoo.com>
2022-01-07 23:13:08 +0400

    add download file

--- PizzaDelivery/PizzaDelivery/Controllers/HomeController.cs
+++ PizzaDelivery/PizzaDelivery/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using PizzaDelivery.Mocks;
 using PizzaDelivery.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,15 +12,28 @@ namespace PizzaDelivery.Controllers
 {
     public class HomeController : Controller
     {
+        [Obsolete]
+        private readonly IHostingEnvironment _appEnvironmrnt;
+        private User user { get; set; }
+        static string firstName;
+        static string lastName;
+
+        [Obsolete]
+        public HomeController(IHostingEnvironment environment)
+        {
+            _appEnvironmrnt = environment;
+        }
         public IActionResult Index()
         {
-            User user = new User();
+
             return View(user);
         }
         public IActionResult Registration(User user, Address address)
         {
             user.MyAdress = new Address { StreetName = address.StreetName };
 
+            firstName = user.FirstName;
+            lastName = user.LastName;
             return View(user);
         }
 
@@ -29,6 +44,7 @@ namespace PizzaDelivery.Controllers
             return View(pizzeys);
         }
 
+        [Obsolete]
         public IActionResult SetProduct(Product[] products)
         {
             MocksPizzeys pizzeys = new MocksPizzeys();
@@ -36,13 +52,50 @@ namespace PizzaDelivery.Controllers
             {
                 for (int i = 0; i < products.Length; i++)
                 {
-                    if (item.PizzaName== products[i].PizzaName)
+                    if (item.PizzaName == products[i].PizzaName)
                     {
                         products[i].Ingredients = item.Ingredients;
                     }
                 }
             }
-            return View(products);
+            string path = _appEnvironmrnt.ContentRootPath + "/Files/file.txt";
+            System.IO.File.Delete(path);
+
+            FileStream file = new FileStream(path, FileMode.OpenOrCreate);
+            using (StreamWriter streamFile = new StreamWriter(file))
+            {
+                foreach (var item in products)
+                {
+                    streamFile.WriteLine("Name: " + item.PizzaName);
+                    streamFile.WriteLine("Ingredients: " + item.Ingredients);
+                    if (item.IsBitter)
+                    {
+                        streamFile.WriteLine("Is better");
+                    }
+                    else
+                    {
+                        streamFile.WriteLine("Is not better");
+                    }
+                    if (item.Size == 1)
+                    {
+                        streamFile.WriteLine("Size is small: 4 Pieces");
+                    }
+                    else if (item.Size == 2)
+                    {
+                        streamFile.WriteLine("Size is medium: 6 Pieces");
+                    }
+                    else
+                    {
+                        streamFile.WriteLine("Size is large: 8 Pieces");
+                    }
+                }
+                streamFile.WriteLine("File Created !" + DateTime.Now.ToLongDateString());
+            }
+            file.Close();
+
+            string fileName = firstName + "." + lastName + "'s order.txt";
+
+            return PhysicalFile(path, "Application/txt", fileName);
         }
     }
 }
